b02552338d40a47c70d0be3e0d1d02a98cc3eb2c
nkwtnb <n.watanabe0305@gmail.com>
2022-01-05 14:44:30 +0900

    不要import等削除

--- src/components/HeaderLabel.tsx
+++ src/components/HeaderLabel.tsx
@@ -1,15 +1,10 @@
-import styled from "styled-components";
-import { Container, Row, Col } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 type HeaderLabelProps = {
   text: string;
 }
 
 export default function HeaderLabel(props: HeaderLabelProps) {
-  const Contact = styled.section`
-    height: 400px;
-  `;
-
   return (
     <Col>
       <div className="text-center">
