36b6c577dc539496e42d98387deace39abcac6c3
nkwtnb <n.watanabe0305@gmail.com>
2022-07-19 18:03:15 +0900

    favicon修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ export default function MyApp ( {Component, pageProps} ) {
   return (
     <>
       <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/gh-pages-portfolio/profile.png" />
         <title>Naoki's portfolio</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
