f5572a7a3da264cd6729191a35d215027fb2eb69
nkwtnb <n.watanabe0305@gmail.com>
2022-07-19 17:50:33 +0900

    title追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../src/_global.css';
 import React from 'react';
+import Head from 'next/head';
 
 export default function MyApp ( {Component, pageProps} ) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Naoki's portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
