f86ff87666cfbe4958abad817c4906f25b10003e
nkwtnb <n.watanabe0305@gmail.com>
2022-07-19 11:46:11 +0900

    Contactにアドレス追加

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,13 +1,32 @@
 import styled from "styled-components";
+import { Container, Row, Col } from "react-bootstrap";
+import mail from "../../resources/images/mail2image.png";
+import ContentText from "../ContentText";
 
 const ContactWrapper = styled.section`
 min-height: 200px;
-`
+`;
 
-export default function Contact(props: any) {
-
+const Contact = function (props: any) {
   return (
     <ContactWrapper>
+      <Container fluid="md" className="h-100">
+        <Row>
+          <Col className="justify-content-center d-flex mt-2">
+            <ContentText align="center" text={`ご興味がありましたら、以下メールアドレスへご連絡ください。
+              ※スパム対策の為、画像となっております。
+              `
+            } />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="justify-content-center d-flex mt-2">
+            <img src={mail.src}/>
+          </Col>
+        </Row>
+      </Container>
     </ContactWrapper>
   );
 }
+
+export default Contact;
